2b2479c54cf9d99e6c452a77c41474a480d63f06
LjGww <lj011@yahoo.com>
2014-01-16 13:32:56 +0100

    Creation
    First Upload

---
+++ AsciiLogViewer.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+'''
+    Textual log viewer
+
+    Usage: AsciiLogViewer filename
+
+    if file name is not provided, it shall show navigator
+
+'''
+__author__ = 'LjGww'
+application_title = "Text Log Viewer"
+
+import sys
+from Tkinter import *
+import tkMessageBox
+import tkFileDialog
+
+def ShowNavigator():
+    return ""
+
+################################################################
+#  Main window
+################################################################
+
+class Application(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack(expand=1, fill=BOTH)
+        self.createWidgets()
+        if len(sys.argv)==1:
+            self.showNavigator()
+        if len(sys.argv)==2:
+            # display file name
+            self.viewFile()
+
+    def showNavigator(self):
+        pass
+
+    def viewFile(self):
+        self.desc['text'] += sys.argv[1]
+
+
+
+    def createWidgets(self):
+		self.desc = Label(self,text='Viewing file: ')
+		self.desc['anchor']="w"
+		self.desc['justify']="left"
+		self.desc.pack(side="top",fill=X)
+
+		self.OutFrame = Frame(self)
+
+		self.outT = Text(master=self.OutFrame)
+		self.outT.pack(fill=BOTH, expand=1, side="left")
+
+		self.scrollbar1 = Scrollbar(master=self.OutFrame,orient=VERTICAL)
+		self.scrollbar1.pack(side="right", fill="y")
+
+		self.outT["yscrollcommand"] = self.scrollbar1.set
+		self.scrollbar1["command"]=self.outT.yview
+
+		self.OutFrame.pack(expand=1, fill=BOTH)
+
+		self.doClose = Button(self)
+		self.doClose["text"] = "Close"
+		self.doClose["command"] = self.master.destroy
+		# self.doClose.pack({"side": "left"})
+		self.doClose.pack(fill=X)
+
+
+################################################################
+#  Application Startup
+################################################################
+
+
+if __name__=="__main__":
+    if len(sys.argv)==1 or len(sys.argv)==2:
+        root = Tk()
+        root.title(application_title)
+        app = Application(master=root)
+        app.mainloop()
+        try:
+	        root.destroy()
+        except:
+            pass
+    else:
+        print __doc__
+        print raw_input("Press Enter to continue...")
+else:
+    # not used as module
+    pass
