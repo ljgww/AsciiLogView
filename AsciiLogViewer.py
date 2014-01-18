7410884b717df8d3914dc1c801b02bde825985e2
ljgww <lj011@yahoo.com>
2014-01-18 20:01:35 +0100

    adding functions

--- AsciiLogViewer.py
+++ AsciiLogViewer.py
@@ -10,12 +10,63 @@
 
 __author__ = 'LjGww'
 application_title = "Text Log Viewer"
+view_full_filename=""
+view_path_only=""
+view_filename_only=""
+
 
 import sys
 import os
 from Tkinter import *
 import tkMessageBox
 import tkFileDialog
+
+
+
+
+
+################################################################
+#  File label popup window
+################################################################
+
+class FilePopup(Toplevel):
+
+    def __init__(self, master=None):
+        Toplevel.__init__(self,master=root)
+        self.resizable(0,0)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.doCopyFullPath = Button(self)
+        self.doCopyFullPath["text"] = "Copy full path file name"
+        self.doCopyFullPath["command"] = self.full
+        self.doCopyFullPath.pack(fill=X)
+
+        self.doCopyPathOnly = Button(self)
+        self.doCopyPathOnly["text"] = "Copy path only"
+        self.doCopyPathOnly["command"] = self.path_only
+        self.doCopyPathOnly.pack(fill=X)
+
+        self.doCopyFileOnly = Button(self)
+        self.doCopyFileOnly["text"] = "Copy file name only"
+        self.doCopyFileOnly["command"] = self.fn_only
+        self.doCopyFileOnly.pack(fill=X)
+
+        self.doClose = Button(self)
+        self.doClose["text"] = "Close"
+        self.doClose["command"] = self.destroy
+        self.doClose.pack(fill=X)
+
+    def full(self):
+        root.clipboard_clear()
+        root.clipboard_append(view_full_filename)
+        self.destroy()
+
+    def path_only(self):
+        self.destroy()
+
+    def fn_only(self):
+        self.destroy()
 
 
 ################################################################
@@ -43,11 +94,22 @@ class Application(Frame):
             self.master.destroy()
 
     def viewFile(self,fname):
-        self.status_line['text'] += fname
+        global view_full_filename
+        view_full_filename = fname
+        self.status_line['text'] += view_full_filename
         if os.path.isfile(fname):
             f = open(fname,"rt")
             for line in f:
                 self.outT.insert(END,line)
+
+    def status_left_click(self,event):
+        # event structure is not interesting for use for this, it is enough to catch an event
+        global view_full_filename
+        root.clipboard_clear()
+        root.clipboard_append(view_full_filename)
+
+    def status_right_click(self,event):
+        self.fpw = FilePopup(master=root)
 
     def createWidgets(self):
         # intention to replace with find box
@@ -88,6 +150,10 @@ class Application(Frame):
         self.status_line['anchor']="w"
         self.status_line['justify']="left"
         self.status_line.pack(fill=X)
+
+        # labels do not have click handler, but we can observe event and act on it
+        self.status_line.bind("<Button-1>", self.status_left_click)
+        self.status_line.bind("<Button-2>", self.status_right_click)
 
 
 
